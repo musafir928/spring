e6a3c88d936a150144ac7153e6a033a6134a8d23
musafir928 <uganoghli@gmail.com>
2022-02-18 23:20:09 +0100

    add stereotype_annotation package

---
+++ Spring-01-Core-IOC/src/main/java/com/cydeo/stereotype_annotation/Agile.java
@@ -0,0 +1,10 @@
+package com.cydeo.stereotype_annotation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Agile {
+    public void getTeachingHours(){
+        System.out.println("Teaching hours: 20/30");
+    };
+}
---
+++ Spring-01-Core-IOC/src/main/java/com/cydeo/stereotype_annotation/Java.java
@@ -0,0 +1,10 @@
+package com.cydeo.stereotype_annotation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Java {
+    public void getTeachingHours(){
+        System.out.println("Teaching hours: 200/300");
+    };
+}
