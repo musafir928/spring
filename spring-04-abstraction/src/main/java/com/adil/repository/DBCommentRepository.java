17011b7fe6d1ccff7aed058bb02de80a27e336ea
musafir928 <uganoghli@gmail.com>
2022-02-24 17:44:59 +0100

    add spring stereotype annotation

--- spring-04-abstraction/src/main/java/com/adil/repository/DBCommentRepository.java
+++ spring-04-abstraction/src/main/java/com/adil/repository/DBCommentRepository.java
@@ -1,7 +1,9 @@
 package com.adil.repository;
 
 import com.adil.model.Comment;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DBCommentRepository implements CommentRepository{
 
     @Override
--- spring-04-abstraction/src/main/java/com/adil/service/CommentService.java
+++ spring-04-abstraction/src/main/java/com/adil/service/CommentService.java
@@ -1,5 +1,22 @@
 package com.adil.service;
 
-public class CommentService {
+import com.adil.model.Comment;
+import com.adil.proxy.CommentNotificationProxy;
+import com.adil.repository.CommentRepository;
+import org.springframework.stereotype.Component;
 
+@Component
+public class CommentService {
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment (Comment comment) {
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
 }
