d306d4f346f3c7903a38cd7970d92d1dd5e19451
musafir928 <uganoghli@gmail.com>
2022-02-25 17:41:39 +0100

    add spring boot demo

---
+++ spring-06-SpringBoot-Demo/src/test/java/com/adil/Spring06SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring06SpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
