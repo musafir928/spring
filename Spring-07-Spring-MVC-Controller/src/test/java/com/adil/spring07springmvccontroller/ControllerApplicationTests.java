9f1356b2627e57bf24994efd2a6a01dc4871fc4f
musafir928 <uganoghli@gmail.com>
2022-03-04 17:08:58 +0100

    add Spring MVC basics

---
+++ Spring-07-Spring-MVC-Controller/src/test/java/com/adil/spring07springmvccontroller/ControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adil.spring07springmvccontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
