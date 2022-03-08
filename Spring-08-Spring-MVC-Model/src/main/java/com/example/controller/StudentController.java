3a036741a79a6be182ab01efea61da9899fe5923
musafir928 <uganoghli@gmail.com>
2022-03-08 22:06:18 +0100

    add class level link

--- Spring-08-Spring-MVC-Model/src/main/java/com/example/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/example/controller/StudentController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Random;
 
 @Controller
+@RequestMapping("/student")
 public class StudentController {
     @RequestMapping("/welcome")
     public String homePage(Model model) {
