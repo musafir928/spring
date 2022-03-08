c5556f5ebf19a64fb56529051757d9c8e314ab79
musafir928 <uganoghli@gmail.com>
2022-03-08 20:33:37 +0100

    ArrayList to ul>li

--- Spring-08-Spring-MVC-Model/src/main/java/com/example/springModel/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/example/springModel/controller/StudentController.java
@@ -4,12 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.*;
+
 @Controller
 public class StudentController {
     @RequestMapping("/welcome")
     public String homePage(Model model) {
         model.addAttribute("name", "adil");
         model.addAttribute("course", "MVC");
+        Random random = new Random();
+        model.addAttribute(
+                "studentIds",
+                Arrays.asList(random.nextInt(),random.nextInt(),random.nextInt(),random.nextInt())
+        );
         return "student/welcome";
     }
 }
