d4dd5820c7c58d027517c57952f4df7d88f33238
musafir928 <uganoghli@gmail.com>
2022-03-16 21:48:58 +0100

    add student model and data generator

---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/adil/bootstrap/DataGenerator.java
@@ -0,0 +1,21 @@
+package com.adil.bootstrap;
+
+import com.adil.model.Student;
+import com.github.javafaker.Faker;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DataGenerator {
+    public static List<Student> createStudent() {
+        List<Student> students = Arrays.asList(
+                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20,60), new Faker().address().state()),
+                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20,60), new Faker().address().state()),
+                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20,60), new Faker().address().state()),
+                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20,60), new Faker().address().state()),
+                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20,60), new Faker().address().state())
+        );
+
+        return students;
+    }
+}
