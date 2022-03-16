d0f2cbe21eca04d216df0881608abf9cf878c799
musafir928 <uganoghli@gmail.com>
2022-03-16 20:43:27 +0100

    add basic MVC

--- Spring-08-Spring-MVC-Model/src/main/java/com/example/controller/RequestParameterController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/example/controller/RequestParameterController.java
@@ -2,8 +2,11 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.websocket.server.PathParam;
 
 @Controller
 @RequestMapping("/car")
@@ -17,6 +20,13 @@ public class RequestParameterController {
     @RequestMapping("/info2")     //localhost:8080/car/info?make=Toyota
     public String catInfo2(@RequestParam(defaultValue = "unknown", required = false) String make, Model model) {
         model.addAttribute("make", make);
+        return "car/car-info";
+    }
+
+    @RequestMapping("info3/{make}/{year}")
+    public String carInfo3(@PathVariable String make, @PathVariable Integer year, Model model) {
+        model.addAttribute("make", make);
+        model.addAttribute("year", year);
         return "car/car-info";
     }
 }
