9b9698754102232a3d53a47f2f74d8de455f9cb9
musafir928 <uganoghli@gmail.com>
2022-03-16 22:53:24 +0100

    thymeleaf data binding

---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/adil/controller/StudentController.java
@@ -0,0 +1,18 @@
+package com.adil.controller;
+
+import com.adil.bootstrap.DataGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+    @RequestMapping("/register")
+    public String register(Model model, @RequestParam(defaultValue = "unknown", required = false) String teacher) {
+        model.addAttribute("students", DataGenerator.createStudent());
+        model.addAttribute("teacher", teacher);
+        return "student/register";
+    }
+}
