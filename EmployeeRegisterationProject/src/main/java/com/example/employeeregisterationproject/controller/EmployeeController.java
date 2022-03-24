b58528c4d25e22d83d0d775459462b2a1c7c7635
musafir928 <uganoghli@gmail.com>
2022-03-24 23:16:07 +0100

    bind form data

--- EmployeeRegisterationProject/src/main/java/com/example/employeeregisterationproject/controller/EmployeeController.java
+++ EmployeeRegisterationProject/src/main/java/com/example/employeeregisterationproject/controller/EmployeeController.java
@@ -1,14 +1,22 @@
 package com.example.employeeregisterationproject.controller;
 
+import com.example.employeeregisterationproject.bootstrap.DataGenerator;
+import com.example.employeeregisterationproject.model.Employee;
+import lombok.Data;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
+
     @GetMapping("/register")
-    public String createUser() {
+    public String createUser(Model model) {
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("states", DataGenerator.getAllStates());
         return "/employee/create-employee";
     }
+
 }
