2a0c94be53bf54571b07bbf20aeb8f2a99ec6b6b
musafir928 <uganoghli@gmail.com>
2022-03-24 22:38:05 +0100

    add form registration

--- Spring-09-Spring-Thymeleaf/src/main/java/com/adil/controller/MentorController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/adil/controller/MentorController.java
@@ -3,8 +3,7 @@ package com.adil.controller;
 import com.adil.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 
@@ -13,8 +12,15 @@ import java.util.Arrays;
 public class MentorController {
     @GetMapping("/register")
     public String showForm(Model model) {
-        model.addAttribute("batchList", Arrays.asList("JD1", "JD2", "EU1","EU2", "B18", "B20"));
+        model.addAttribute("batchList", Arrays.asList("JD1", "JD2", "EU1", "EU2", "B18", "B20"));
         model.addAttribute("mentor", new Mentor());
         return "mentor/mentor-register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute("mentor") Mentor mentor, Model model) {
+//        model.addAttribute("mentor", mentor);
+        return "redirect:/mentor/register";
+//        return "mentor/mentor-creation";
     }
 }
