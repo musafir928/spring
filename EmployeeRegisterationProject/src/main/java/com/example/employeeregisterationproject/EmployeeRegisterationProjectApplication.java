f601bbffc7687ea19c14efe50bf67330a2e676f9
musafir928 <uganoghli@gmail.com>
2022-04-15 00:21:43 +0200

    ORM setup

---
+++ EmployeeRegisterationProject/src/main/java/com/example/employeeregisterationproject/EmployeeRegisterationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeeregisterationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRegisterationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeRegisterationProjectApplication.class, args);
+    }
+
+}
---
+++ EmployeeRegisterationProject/src/test/java/com/example/employeeregisterationproject/EmployeeRegisterationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeeregisterationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegisterationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
