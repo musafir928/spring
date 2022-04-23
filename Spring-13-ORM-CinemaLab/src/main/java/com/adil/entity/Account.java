f67324302a67bf87bfa1a9cb42e516e3f26be6c8
musafir928 <uganoghli@gmail.com>
2022-04-23 17:37:11 +0200

    add account entity

--- Spring-13-ORM-CinemaLab/src/main/java/com/adil/entity/Account.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/adil/entity/Account.java
@@ -4,9 +4,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
 @Data
 @NoArgsConstructor
+@Table(name = "account_details")
 public class Account extends BaseEntity{
+    private String name;
+    private String address;
+    private String country;
+    private String city;
+    private String state;
+    private int age;
+
+
 }
