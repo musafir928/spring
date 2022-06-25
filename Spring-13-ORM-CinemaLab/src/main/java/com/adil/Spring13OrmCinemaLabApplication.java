2670ed1564cee5f78ae3322c3484561b9cbc05b9
musafir928 <uganoghli@gmail.com>
2022-06-25 16:20:24 +0200

    add AOP

---
+++ Spring-13-ORM-CinemaLab/src/main/java/com/adil/Spring13OrmCinemaLabApplication.java
@@ -0,0 +1,25 @@
+package com.adil;
+
+import com.adil.repository.AccountRepository;
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.output.MigrateResult;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class Spring13OrmCinemaLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring13OrmCinemaLabApplication.class, args);
+    }
+
+//    @Bean
+//    public MigrateResult migrateResult(DataSource dataSource){
+//        return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+//    }
+
+}
---
+++ Spring-17-Consuming-apis/src/test/java/com/cydeo/spring17consumingapis/Spring17ConsumingApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring17consumingapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring17ConsumingApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
