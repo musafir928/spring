d999cb060d39dc4dbdcd31bde0046ef0eba400ec
musafir928 <uganoghli@gmail.com>
2022-07-02 21:35:19 +0200

    add basic junit unit test

---
+++ Spring-21-junit5-unittesting/src/test/java/CalculatorTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void testCase1() {
+
+    }
+    @Test
+    void testCase2() {
+
+    }
+    @Test
+    void testCase3() {
+
+    }
+    @Test
+    void testCase4() {
+
+    }
+    @Test
+    void testCase5() {
+
+    }
+    @Test
+    void add() {
+        assertEquals(5,Calculator.add(2,3));
+    }
+}
