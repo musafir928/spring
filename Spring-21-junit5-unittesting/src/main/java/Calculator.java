673c5868d93a2b458f89fc031abca1dadb554bc9
musafir928 <uganoghli@gmail.com>
2022-07-02 21:53:43 +0200

    add before/after hooks

--- Spring-21-junit5-unittesting/src/main/java/Calculator.java
+++ Spring-21-junit5-unittesting/src/main/java/Calculator.java
@@ -3,4 +3,9 @@ public class Calculator {
     public static int add(int num1, int num2) {
         return num1+num2;
     }
+
+    public static int add2(int num1, int num2) throws IllegalAccessException {
+        if(num1>num2) {throw new IllegalAccessException();}
+        return num1+num2;
+    }
 }
